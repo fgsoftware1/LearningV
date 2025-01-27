92bbb4cc730e8f1e1f16d87fbeb9f9e6ecc495a5
babywolf <60277618+fgsoftware1@users.noreply.github.com>
2025-01-27 09:31:26 +0000

    first commit

---
+++ variables.v
@@ -0,0 +1,12 @@
+module main
+
+fn main(){
+	s := "Hello World!"
+	x := 1
+	d := 2.2
+
+	println("Printing variables")
+	println("Printing text: ${s}")
+	println("Printing integer: ${x}")
+	println("Printing decimal: ${d}")
+}
